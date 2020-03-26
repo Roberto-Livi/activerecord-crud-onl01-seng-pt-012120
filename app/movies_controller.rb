6d30788c87ca652190059bc648ffa12f58e5dd27
Roberto Livi <robertomlivi@outlook.com>
2020-03-26 19:13:57 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -29,8 +29,8 @@ def can_be_created_in_a_block(title: "Home Alone", release_date: 1990)
   # release_date == 1990
   
   Movie.create do |m|
-    m.string :title
-    m.integer :release_date
+    m.title = "The Room"
+    m.release_date = 1990
   end
 end
 
